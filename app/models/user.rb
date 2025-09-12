251190133cc77b935986f43017ccdafac6d30a1e
Raphael-Maximowski <raphaelmxjv@gmail.com>
2025-09-12 10:11:56 -0300

    feat: users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,17 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint           not null, primary key
+#  email      :string(255)      not null
+#  name       :string(255)      not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class User < ApplicationRecord
+    validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }, length: { maximum: 100 }
+    validates :name, presence: true, length: { maximum: 50 }
+
     has_many :initiated_chats, class_name: 'Chat', foreign_key: 'initializer_id'
     has_many :received_chats, class_name: 'Chat', foreign_key: 'receptor_id'
     
--- test/models/chat_test.rb
+++ test/models/chat_test.rb
@@ -1,3 +1,23 @@
+# == Schema Information
+#
+# Table name: chats
+#
+#  id             :bigint           not null, primary key
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#  initializer_id :bigint           not null
+#  receptor_id    :bigint           not null
+#
+# Indexes
+#
+#  fk_chat_initializer_id  (initializer_id)
+#  fk_chat_receptor_id     (receptor_id)
+#
+# Foreign Keys
+#
+#  fk_chat_initializer_id  (initializer_id => users.id)
+#  fk_chat_receptor_id     (receptor_id => users.id)
+#
 require "test_helper"
 
 class ChatTest < ActiveSupport::TestCase
